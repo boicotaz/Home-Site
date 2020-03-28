26f86d4950c39077f2dacc281b5703363a802ce4
steliosKon <steliosnteloss@gmail.com>
2020-03-28 03:27:29 +0200

    new navbar/added fontawesome header

--- components/navbar/NavigationBarComponent.jsx
+++ components/navbar/NavigationBarComponent.jsx
@@ -1,118 +1,102 @@
 import { Link } from "react-router-dom";
+import React from 'react';
 
-function NavigationBar() {
-  return (
-    <React.Fragment>
-      <nav className="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">
-          {/* Brand */}
-          <img
-            src={"../../public/room8s_logo.png"}
-            alt="logo"
-            style={{ width: "60px" }}
-          />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mx-auto">
-            <li className="nav-item active">
-            <a className="nav-link">
-              <Link style={{color:'white'}} to="/home"><i className="fa fa-home fa-2x"></i></Link>
-              {/* 
-                <span className="sr-only">(current)</span>
-              */}</a> 
-            </li>
-            <li className="nav-item">
-            <a className="nav-link" href="/expenses">
-              <Link style={{color:'white'}} to="/expenses"><i className="fa fa-usd fa-2x" aria-hidden="true"></i></Link>
-                  {/* <span className="badge badge-danger"></span> */}
-              </a> 
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                <Link style={{color:'white'}} to ="/expenses"><i className="fa fa-sticky-note-o fa-2x" aria-hidden="true">
-                  {/* <span className="badge badge-warning"></span> */}
-                </i> </Link>
-                {/* Notes */}
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="/"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                <i style={{color:'white'}} className="fa fa-bell fa-2x" aria-hidden="true">
-                  {/* <span className="badge badge-primary"></span> */}
-                </i>
-                {/* Notifications */}
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="/">
-                  Action
-            </a>
-                <a className="dropdown-item" href="/">
-                  Another action
-            </a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="/">
-                  Something else here
-            </a>
-              </div>
-            </li>
-          </ul>
-          <ul className="navbar-nav ">
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                <i className="fa fa-user-circle-o">
-                  <span className="badge badge-info"></span>
-                </i>
-                {/* Profile */}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                <i className="fa fa-sign-out">
-                  <span className="badge badge-success"></span>
-                </i>
-                {/* Logout */}
-              </a>
-            </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input
-              className="form-control mr-sm-2"
-              type="text"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button
-              className="btn btn-outline-success my-2 my-sm-0"
-              type="submit"
-            >
-              Search
-        </button>
-          </form>
-        </div>
-      </nav>
-    </React.Fragment>
+class NavigationBar extends React.Component{
 
-  );
-}
+  render(){
+    return( 
+
+<React.Fragment>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light w-100">
+
+  {/* Nav Brand */}
+    <a className="navbar-brand" href="#">
+      <img
+        src={"../../public/room8s_logo.png"}
+        width="40"  
+        height="40"
+        className="d-inline-block align-top"
+        alt="room8s logo"
+      />
+    </a>
+{/* Burger Button */}
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+    
+  {/* Side Menu and Search Bar */}
+
+<div className="navbar-nav nav-tabs nav-justified" ></div>
+  <div className="dropdown">
+    <a className="btn btn-default dropdown-toggle" href="#" role="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-label="Settings">
+      <i className="fas fa-users-cog"></i>
+    </a>
+
+  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a className="dropdown-item" href="#">Profile</a>
+    <a className="dropdown-item" href="#">Settings</a>
+    <a className="dropdown-item" href="#">Logout</a>
+  </div>  
+  </div>
+
+        <form className="form-inline my-2 my-lg-0">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search Users" aria-label="Search"/>
+        </form>
+      {/* </Nav> */}  
+    </div>
+  </nav>
+
+
+
+{/* Main Menu in the center of the page */}
+<div className="container">
+<div className="collapse navbar-collapse" id="navbarSupportedContent">
+<ul className="nav nav-tabs nav-justified" >
+  <li className="nav-item ative">
+  <Link style={{color:"#003300"}} to="/home"><a data-toggle="tab" className="nav-link " >Home</a></Link>
+  </li>
+  <li className="nav-item">
+  <Link style={{color:"#990000"}} to="/expenses"><a data-toggle="tab"  className="nav-link" > Expenses</a></Link>
+  </li>
+  <li className="nav-item">
+  <Link style={{color:"#e6ac00"}} to="/calendar"> <a  data-toggle="tab" className="nav-link" > Calendar</a></Link>
+  </li>
+  <li className="nav-item">
+  <Link style={{color:"black"}} to="/notifications"><a data-toggle="tab" className="nav-link" > Notifications</a></Link>
+  </li>
+</ul>
+
+<div className="tab-content ">
+			  <div className="tab-pane active" id="1">
+          <h3>Standard tab panel created on bootstrap using nav-tabs</h3>
+				</div>
+				<div className="tab-pane" id="2">
+          <h3>Notice the gap between the content and tab after applying a background color</h3>
+				</div>
+        <div className="tab-pane" id="3">
+          <h3>add clearfix to tab-content (see the css)</h3>
+				</div>
+			</div>
+  
+
+<hr></hr>
+
+  
+</div>
+</div>
+  <br>
+</br>
+
+
+</React.Fragment> )
+
+}}
+
+
+ 
 
 export default NavigationBar;
