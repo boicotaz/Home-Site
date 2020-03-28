ab2b3225e4fc67b8f344d1187d27812bf9f39196
steliosKon <steliosnteloss@gmail.com>
2020-03-28 21:29:53 +0200

    no changes

--- components/AppComponent.jsx
+++ components/AppComponent.jsx
@@ -16,9 +16,7 @@ export default class App extends React.Component {
     this.state.groupMessages = this.props.groupMessages;
     this.state.currentUser = this.props.currentUser;
     this.state.loggedInMembersId = [];
-
     this.state.usersInGroup = this.props.usersInGroup;
-
     this.state.view = this.props.view;
     this.state.expenses = this.props.expenses;
     this.state.totals = this.props.totals;
@@ -99,11 +97,24 @@ export default class App extends React.Component {
           <Switch>
 
             <Route exact path="/home" render={(props) => {
-              return <MainPage  {...props} groupDetails={this.state.groupDetails} groupMessages={this.state.groupMessages} usersInGroup={this.state.usersInGroup} currentUser={this.state.currentUser} loggedInMembersId={this.state.loggedInMembersId}   > </MainPage>
+              return <MainPage
+                {...props} 
+              groupDetails={this.state.groupDetails} 
+              groupMessages={this.state.groupMessages} 
+              usersInGroup={this.state.usersInGroup} 
+              currentUser={this.state.currentUser}
+              loggedInMembersId={this.state.loggedInMembersId}>
+                 </MainPage>
             }} />
 
             <Route exact path="/expenses" render={(props) => {
-              return <ExpensesPage  {...props} view={this.state.view} expenses={this.state.expenses} totals={this.state.totals} usersInGroupDetails={this.state.usersInGroup}  > </ExpensesPage>
+              return <ExpensesPage 
+               {...props}
+                view={this.state.view}
+                expenses={this.state.expenses}
+                totals={this.state.totals}
+                usersInGroupDetails={this.state.usersInGroup} >
+                  </ExpensesPage>
             }} />
 
           </Switch>
