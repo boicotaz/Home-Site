543ab128c1c89f8f5ddc3707e0825babe2399400
steliosKon <steliosnteloss@gmail.com>
2020-03-28 03:31:56 +0200

    fixed

--- components/navbar/NavigationBarComponent.jsx
+++ components/navbar/NavigationBarComponent.jsx
@@ -27,10 +27,10 @@ class NavigationBar extends React.Component{
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-    
+  
   {/* Side Menu and Search Bar */}
 
-<div className="navbar-nav nav-tabs nav-justified" ></div>
+<div className="navbar-nav ml-auto" ></div>
   <div className="dropdown">
     <a className="btn btn-default dropdown-toggle" href="#" role="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-label="Settings">
       <i className="fas fa-users-cog"></i>
@@ -53,41 +53,27 @@ class NavigationBar extends React.Component{
 
 
 {/* Main Menu in the center of the page */}
-<div className="container">
-<div className="collapse navbar-collapse" id="navbarSupportedContent">
-<ul className="nav nav-tabs nav-justified" >
-  <li className="nav-item ative">
-  <Link style={{color:"#003300"}} to="/home"><a data-toggle="tab" className="nav-link " >Home</a></Link>
+<div class="container">
+<ul class="nav nav-tabs nav-justified">
+  <li class="nav-item">
+  <Link style={{color:"#003300"}} to="/home"><a  class="nav-link " >Home</a></Link>
   </li>
-  <li className="nav-item">
-  <Link style={{color:"#990000"}} to="/expenses"><a data-toggle="tab"  className="nav-link" > Expenses</a></Link>
+  <li class="nav-item">
+  <Link style={{color:"#990000"}} to="/expenses"><a  class="nav-link" > Expenses</a></Link>
   </li>
-  <li className="nav-item">
-  <Link style={{color:"#e6ac00"}} to="/calendar"> <a  data-toggle="tab" className="nav-link" > Calendar</a></Link>
+  <li class="nav-item">
+  <Link style={{color:"#e6ac00"}} to="/calendar"> <a  class="nav-link" > Calendar</a></Link>
   </li>
-  <li className="nav-item">
-  <Link style={{color:"black"}} to="/notifications"><a data-toggle="tab" className="nav-link" > Notifications</a></Link>
+  <li class="nav-item">
+  <Link style={{color:"black"}} to="/notifications"><a class="nav-link" > Notifications</a></Link>
   </li>
 </ul>
-
-<div className="tab-content ">
-			  <div className="tab-pane active" id="1">
-          <h3>Standard tab panel created on bootstrap using nav-tabs</h3>
-				</div>
-				<div className="tab-pane" id="2">
-          <h3>Notice the gap between the content and tab after applying a background color</h3>
-				</div>
-        <div className="tab-pane" id="3">
-          <h3>add clearfix to tab-content (see the css)</h3>
-				</div>
-			</div>
-  
-
-<hr></hr>
-
   
 </div>
-</div>
+
+
+
+
   <br>
 </br>
 
