f61cd695c4988f4550611a9cc37815173dec4616
steliosKon <steliosnteloss@gmail.com>
2020-03-29 15:01:14 +0300

    updated navbar, added tooltip/active-state/logout/

--- components/navbar/NavigationBarComponent.jsx
+++ components/navbar/NavigationBarComponent.jsx
@@ -1,120 +1,87 @@
-import { Link,NavLink } from "react-router-dom";
+import {NavLink } from "react-router-dom";
 import React from 'react';
-import "./nav.css"
-
-// <Link to="/">
-//</Link>
- 
-
-
+import '../../public/css/NavigationBarComponent.css'
 
 class NavigationBar extends React.Component{
 
-  render(){
-    return( 
-<React.Fragment>
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    render(){
+      return( 
+  <React.Fragment>
   
-  <a className="navbar-brand" href="#"><img
-        src={"../../public/room8s_logo.png"}
-        width="40"  
-        height="40"
-        className="d-inline-block align-top"
-        alt="room8s logo"
-      />
+  <nav className="navbar fixed-top navbar-expand-md navbar-dark bg-dark justify-content-between">
       
-  </a>
-<button 
-  className="navbar-toggler" 
-  type="button" 
-  data-toggle="collapse" 
-  data-target="#navbarToggler" 
-  aria-controls="navbarToggler" 
-  aria-expanded="false" 
-  aria-label="Toggle navigation">
-  <span className="navbar-toggler-icon"></span>
-</button>
-
-
-
-<div className="collapse navbar-collapse" id="navbarToggler">
-  <ul className="nav nav-tabs mx-auto mt-2 mt-lg-0" id="myTabJust" role="tablist">
-
-    <li className="nav-item">
-      <NavLink exact activeClassName="selected" to="/home">
-        <a className="nav-link"> 
-          <i className="fas fa-home">
-          </i>
-        </a>
-      </NavLink>
-    </li>
-
-    <li className="nav-item">
-      <NavLink exact activeClassName="selected" to="/expenses">
-        <a className="nav-link">
-          <i className="fas fa-wallet">
-          </i>
-        </a>
-      </NavLink>
-    </li>
-
-
-    <li className="nav-item">
-      <NavLink exact activeClassName="selected" to="/home">
-        <a className="nav-link">
-          <i className="far fa-calendar-alt">
-          </i>
-        </a>
-      </NavLink>
-    </li>
-
-    <li className="nav-item">
-      <NavLink exact activeClassName="selected" to="/expenses">
-        <a className="nav-link">
-          <i className="fas fa-bell">
-          </i>
-        </a>
-      </NavLink>
-    </li>
-
-
-
-  <li className="nav-item">
-    <NavLink exact activeClassName="selected" to="/home">
-      <a className="nav-link" >
-        <i className="fas fa-tasks">
-        </i>
-      </a>
-    </NavLink>
-  </li> 
-</ul>
-
-
-
-    <form class="form-inline my-2 my-lg-0">
-    <div className="dropdown my-2 my-sm-0">
-    <a className="btn btn-default dropdown-toggle" href="#" role="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-label="Settings">
-      <i className="fas fa-users-cog"></i>
-    </a>
-
-  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a className="dropdown-item" href="#">Profile</a>
-    <a className="dropdown-item" href="#">Settings</a>
-    <a className="dropdown-item" href="#">Logout</a>
-  </div>  
-  </div>
-
-      <input className="form-control mr-sm-2" type="search" placeholder="Search Users" aria-label="Search"/>
-   
-    </form>
-  </div>
-</nav>
-
-
-</React.Fragment> )
-}}
-
-
- 
-
-export default NavigationBar;
+      <a href="/" className="navbar-brand"><img
+          src={"../../public/room8s_logo.png"}
+          width="40"  
+          height="40"
+          className="d-inline-block align-top"
+          alt="room8s logo"
+        /></a>
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+          <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="navbar-collapse collapse justify-content-center" id="collapsingNavbar">
+          <ul className="navbar-nav">
+              <li className="nav-item">
+              <NavLink to="/home" activeClassName="selected" >
+                  <i data-toggle="tooltip" title="Home" className="fas fa-home fa-2x"></i> 
+              </NavLink>
+              </li>
+              <li className="nav-item">
+              <NavLink  to="/expenses" activeClassName="selected">
+                  <i data-toggle="tooltip" title="Expenses" className="fas fa-wallet fa-2x"></i>
+                  </NavLink>
+              </li>
+              <li className="nav-item">
+              <NavLink to="/calendar" activeClassName="selected" >
+                  <i data-toggle="tooltip" title="Calendar" className="far fa-calendar-alt fa-2x"></i>
+                  </NavLink>
+              </li>
+              <li className="nav-item" >
+              <NavLink to="/notifications" activeClassName="selected">
+                  <i data-toggle="tooltip" title="Notifications" className="fas fa-bell fa-2x"></i>
+                  </NavLink>
+              </li>
+              <li className="nav-item">
+              <NavLink to="/tasks" activeClassName="selected">
+              <i data-toggle="tooltip" title="Notifications" className="fas fa-tasks fa-2x"></i>
+                  </NavLink>
+              </li>
+          </ul>
+  
+      </div>
+      <ul className="nav navbar-nav flex-row">
+      
+          <li className="nav-item">
+          <NavLink to="/profile" activeClassName="selected">
+          <i data-toggle="tooltip" title="Profile" className="fas fa-user fa-lg"></i>
+          </NavLink></li>
+  
+          <li className="nav-item">
+          <NavLink to="/sign-out" activeClassName="selected">
+          <i data-toggle="tooltip" title="Sign out" href="/sign-out" className="fas fa-sign-out-alt fa-lg"></i>
+          </NavLink></li> 
+      </ul>
+  
+  
+      <form className="form-inline">
+      
+        <input 
+        className="form-control mr-sm-2" 
+        type="search" 
+        placeholder="Search Users"
+        aria-label="Search"
+        />
+      </form>
+  </nav>
+  <br></br>
+  <br></br>
+  <br></br>
+  <br></br>
+  <br></br>
+  <br></br>
+  
+  </React.Fragment> )
+  }}
+  
+  export default NavigationBar;
