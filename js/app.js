18710bd40caaf930a1f7b8edb17f0046d805ace1
steliosKon <steliosnteloss@gmail.com>
2020-04-04 22:09:15 +0300

    typos

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 import App from "../components/AppComponent.jsx";
-import { grouDetailsAjax } from "../ajax/groupDetailsAjax";
+import { groupDetailsAjax } from "../ajax/groupDetailsAjax";
 import { groupMessagesAjax } from "../ajax/groupMessagesAjax";
 import { expensesAjax } from "../ajax/expensesAjax.js"
 
 console.log('DID WE GET THE LOGGED IN USER?', loggedInUser);
 function renderApp () {
 
-  Promise.all([grouDetailsAjax.getUsersInGroupDetails(), grouDetailsAjax.getGroupDetails(), groupMessagesAjax.getGroupMessages(), expensesAjax.getExpenseDataAjax(), expensesAjax.getExpenseTotalsDataAjax()]).then((res) => {
+  Promise.all([groupDetailsAjax.getUsersInGroupDetails(), groupDetailsAjax.getGroupDetails(), groupMessagesAjax.getGroupMessages(), expensesAjax.getExpenseDataAjax(), expensesAjax.getExpenseTotalsDataAjax()]).then((res) => {
     let [usersInGroup, groupDetails, groupMessages, expenseData, expensesTotals] = res;
     let processedData = expensesAjax.processData(expenseData, usersInGroup);
     // console.log(processedData), "PROCESSEDD DATA___________________________";
