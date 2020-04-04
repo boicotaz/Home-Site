ec18a48be75fb8718eb4dd28b4bffd75246d6d69
Apostolis Gerodimos <gerodiak@auth.gr>
2020-04-04 14:07:09 +0300

    Added in Notifications small image that vaguely describes the notification's context

--- components/navbar/NotificationsComponent.jsx
+++ components/navbar/NotificationsComponent.jsx
@@ -38,14 +38,15 @@ export default class Notifications extends React.Component {
                     <i id="bell" data-toggle="tooltip" title="Notifications" className="fas fa-bell fa-2x "></i>
                     <i className="badge badge-danger badge--font" style={newNotificationsStyle}>{this.state.notifications.newNotificationsSum}</i>
                 </div>
-                <div className="dropdown-menu dropdown-menu-right p-0 dropdown-menu--rounded bg-dark" style={{ width: "40rem" }}
+                <div className="dropdown-menu dropdown-menu-right p-0 dropdown-menu--rounded dropdown-menu--custom bg-dark"
                     aria-labelledby="navbarDropdownMenuLink">
                     <span className="dropdown-header dropdown-header--font dropdown-header--grey ">Notifications</span>
 
                     <div className="dropdown-item p-0 pt-3 pb-3 h-100 dropdown-item--custom-width dropdown-item--shadow">
                         <div className="row m-1 p-0 ml-3 align-content-center" style={{ maxHeight: '8rem', overflowY: 'auto', height: "8rem" }}>
                             <div className="col-2 ml-0 pl-0 pr-0">
-                                <img src="/public/38k17x.png" className="img-thumbnail rounded-circle img-pos" alt="UserImg" />
+                                <img src="/public/myimg.jpg" className="img-thumbnail rounded-circle img-pos" alt="UserImg" />
+                                <i className="fas fa-wallet fa-custom"></i>
                             </div>
                             <div className="col-10 p-0">
                                 <p className="text-light rounded m-0" style={{ width: '100%', overflowWrap: "break-word", whiteSpace: "pre-line", fontSize: "1.4rem" }}>Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify</p>
@@ -59,6 +60,7 @@ export default class Notifications extends React.Component {
                         <div className="row m-1 p-0 ml-3 align-content-center" style={{ maxHeight: '10rem', overflowY: 'auto', height: '8rem' }}>
                             <div className="col-2 ml-0 pl-0 pr-0">
                                 <img src="/public/38k17x.png" className="img-thumbnail rounded-circle img-pos" alt="UserImg" />
+                                <i className="far fa-calendar-alt fa-custom"></i>
                             </div>
                             <div className="col-10 p-0">
                                 <p className="text-light rounded m-0" style={{ width: '100%', overflowWrap: "break-word", whiteSpace: "pre-line", fontSize: "1.4rem" }}>Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify Notify</p>
